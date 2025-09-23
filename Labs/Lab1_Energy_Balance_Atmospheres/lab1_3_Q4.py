60ac956c9b3be0439b5ae6f4b6404ae7735255cf
chingy053 <chingy@umich.edu>
2025-09-23 16:24:05 -0400

    Update albedo to 0.8

--- Labs/Lab1_Energy_Balance_Atmospheres/lab1_3_Q4.py
+++ Labs/Lab1_Energy_Balance_Atmospheres/lab1_3_Q4.py
@@ -15,7 +15,7 @@ from lab1_1_nlayer import n_layer_atmos, Stefan_Boltzmann
 #---------------------------------------------------------------
 # 4: Run the model for Venus
 #    Assume the following for Venus:
-#    - Surface temperature = ∼ 700K
+#    - Surface y = ∼ 700K
 #    - S0 = 2600 W/m2
 #    - N atmospheric layers where N > 1
 #    - Emissivity = 1
@@ -24,7 +24,7 @@ from lab1_1_nlayer import n_layer_atmos, Stefan_Boltzmann
 #---------> Calculate the temperature for each layer and plot
 
 # Define the number of layers
-n_4 = np.linspace(2,31,30)
+n_4 = np.linspace(2,151,150)
 temp_4 = np.zeros(len(n_4))
 
 # Loop through each layer
@@ -33,7 +33,7 @@ fig3, ax3 = plt.subplots(figsize=(16,10))
 for kk in range(len(n_4)):
 
     # Get vertical temperature profile
-    temp_4[kk] = Stefan_Boltzmann(n_layer_atmos(int(n_4[kk]), epsilon=1,albedo=0,s0=2600, debug = False))[0]
+    temp_4[kk] = Stefan_Boltzmann(n_layer_atmos(int(n_4[kk]), epsilon=1,albedo=0.8,s0=2600, debug = False))[0]
 
 #---------> Plot!
 ax3.plot(temp_4, n_4, color='b', linewidth=2)
@@ -41,7 +41,7 @@ ax3.axvline(x=700, color='k', linestyle='--')
 ax3.set_xlabel('Temperature (K)')
 ax3.set_ylabel('Number of Layers')
 ax3.set_title(f'How many perfectly absorbing layers would give Venus a surface temperature of 700 K?')
-ax3.text(480, 31, "Solar Irradiance = 2600 W/m$^{2}$, Albedo = 0, Emissivity = 1", fontsize=16, color="k")
+ax3.text(380, 148, "Solar Irradiance = 2600 W/m$^{2}$, Albedo = 0.8, Emissivity = 1", fontsize=16, color="k")
 ax3.grid(True)
 plt.rcParams.update({'font.size': 16})
 fig3.savefig("plot_lab1_3_Q4.png")
